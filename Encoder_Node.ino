6689f2c6a362587df25cdbafbdac2795be3972a4
Paul <paulskirk53@gmail.com>
2018-11-28 18:23:20 +0000

    The Version 2 which does not use AckPayload feature

--- Encoder_Node.ino
+++ Encoder_Node.ino
@@ -1,8 +1,8 @@
 
 
 
-//  Name:       Radio_Receiver1.ino
-//   Created:	09/11/2018 08:46:37
+//  Name:       Two_Way_Encoder
+//   Created:	28/11/2018 08:46:37
 //   Author:     DESKTOP-OCFJAV9\Paul
 
 // Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
@@ -10,7 +10,7 @@
 
 #include <SoftwareSerial.h>
 #include <SPI.h>
-//#include <nRF24L01.h>
+#include <nRF24L01.h>
 #include <RF24.h>
 
 //for nrf to work, pin 10 must be high if it is not used as an nrf connecton
@@ -27,16 +27,17 @@
 RF24 radio(7, 8); // CE, CSN
 
 
-const byte address[6] = "00001";            // 00001 the address of this arduino board/ transmitter
+const byte thisNodeaddress[6] = "00001";            // 00001 the address of this arduino board/ transmitter
+const byte masterNodeaddress[6] = "00000";          // the address of the Master
+char message[8] = ""  ;                              // this data type must be used for radio.write
 
-double message = 1.0;                      // this data type will suit the encoder and holds the value
 //encoder:
-volatile long int A_Counter = 10413 / 2; // this is the mid point of the encoder travel and equates to South -
+volatile long int A_Counter = 10413 / 2;  // this is the mid point of the encoder travel and equates to South -
 // the starting point of the dome must be facing south
-
 long int flag_B = 0;
-String receivedData = "";
 
+//General
+String receivedData = "";
 double Azimuth;                                         // to be returned when a TX call is processed by this arduino board
 
 
@@ -46,16 +47,13 @@ void setup()
   digitalWrite (PIN10, HIGH);            //NEW**********************
   Serial.begin(9600);
 
-
   radio.begin();
-
-
   radio.setChannel(100);
   radio.enableAckPayload();            // enable ack payload - slaves reply with data using this feature
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MIN);
   radio.enableDynamicPayloads();
-  radio.openReadingPipe(1, address);    // the 1st parameter can be any number 0 to 5
+  radio.openReadingPipe(1, thisNodeaddress);    // "000001" the address the master writes to when communicating with this encoder node
   radio.startListening();
 
 
@@ -72,11 +70,13 @@ void setup()
 
 void loop()
 {
+  radio.startListening();
+  delay(20);                    // just in case hardware needs time before next instuction exec
 
   while (!radio.available())
   {
     encoder();
-    message = Azimuth;
+    dtostrf(Azimuth, 7, 2, message); // convert double to char total width 7 with 2 dp
 
 
   }
@@ -85,17 +85,22 @@ void loop()
   if (radio.available())
   {
     char text[32] = "";             // used to store what the master node sent e.g AZ hash SA hash
-    radio.writeAckPayload(1, &message, sizeof(message));  // just populates the buffer, does not send
+    // not required now radio.writeAckPayload(1, &message, sizeof(message));
     radio.read(&text, sizeof(text));
-
+    if (text[0] == 'A' && text[1] == 'Z' && text[2] == '#')
+    {
+      radio.stopListening();
+      radio.openWritingPipe(masterNodeaddress);
+      radio.write(&message, sizeof(message));
+    }
     Serial.print("The text received from Mster was: ");
     Serial.println(text);
     Serial.print("the Azimuth value returned to the master is ");
     Serial.println(message);
     Serial.println("--------------------------------------");
-  }
+  }  //endif radio available
 
-}
+}  // end void loop
 
 void encoder()
 {
