8515eeba59c71c8ca6f89898ac6c2350a8aee264
Paul <paulskirk53@gmail.com>
2022-10-27 18:05:17 +0100

    Revert "important code correction"
    This reverts commit a74a661dc647c11a6fad543808a07b95e475a217.

--- src/Encoder_Node.cpp
+++ src/Encoder_Node.cpp
@@ -102,7 +102,7 @@ void setup()
   pinMode(CameraPower, OUTPUT);
     
   //turn the camera power of at startup:
-  digitalWrite (CameraPower, HIGH);           //  HIGH is camera power OFF (new N channel driver for P-Channel high side switch)
+  digitalWrite (CameraPower, LOW);           //  LOW is camera power OFF
 
   //  notes for serial comms -
   ASCOM.begin(19200);   // with ASCOM driver refer to DIP 40 pinout to get correct pin numbers for all the serial ports - see the google doc - 'Pin config for Radio Encoder MCU'
@@ -333,12 +333,12 @@ bool PowerForCamera(bool State)
 {
   if (State)
   {
-    digitalWrite(CameraPower, LOW);            //  LOW is camera power ON (new N channel driver for P-Channel high side switch)
+    digitalWrite(CameraPower, HIGH);  //
     cameraPowerState = on;
   }
   else
   {
-    digitalWrite(CameraPower, HIGH);           //  HIGH is camera power OFF (new N channel driver for P-Channel high side switch)
+    digitalWrite(CameraPower, LOW); //NB as above
     cameraPowerState = off;
   }
 }
